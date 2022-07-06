8ec003949363ebe5a2ce69c073741957556b0b59
Valeria <valeriareveles12@gmail.com>
2022-07-06 10:22:28 -0500

    trying to get javascript functionalality to work

--- js/main.js
+++ js/main.js
@@ -1,44 +1,5 @@
 "use strict"
-
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    // html += '<lh>' + coffee.id + '</lh>';
-    html += '<h3>' + coffee.name + '</h3>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
-
-    return html;
-}
-
-function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]); /*trying a sort here but didn't work*/
-    }
-    return html;
-}
-function filterCoffees() {
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = []; /* tried a sort here but didnt work*/
-    // do you match the roast selection, and the pattern youre typing in?
-
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    return filteredCoffees
-}
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var filteredCoffees = filterCoffees()
-    divListSorted.innerHTML = renderCoffees(filteredCoffees); /*Tried an array sort here too*/
-}
-
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -54,26 +15,102 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 
-]; /*Tried a sort here but didnt work*/
+];
+const divListSorted = document.getElementById('coffees'); /* Tried a sort here too*/
+const nameSelection = document.getElementById("search-name");
+const roastSelection = document.getElementById('roast-selection');
+const coffeeSubmit = document.getElementById('submit');
+
+roastSelection.addEventListener('change', renderCoffeeList);
+nameSelection.addEventListener("input" ,renderCoffeeList);
+coffeeSubmit.addEventListener('click', (e) =>{
+    e.preventDefault();
+    console.log("click");
+});
+function renderCoffeeList(coffees) {
+    let list = coffees;
+    let nameValue = nameSelection.value;
+    let roastValue = roastSelection.value;
+    console.log(nameValue + "\n" + roastValue);
+    if (roastValue !== "all" ){
+        list = list.filter(function (coffee) {
+            coffee.roast === roastValue
+        });}
+    if (nameValue){
+        list = list.filter(function (coffee) {
+            coffee.name.toLowerCase().includes(nameValue.toLowerCase())
+        });}
+    list = list.map(function (coffee) {
+        `<h3>${nameValue}</h3> <p>${roastValue}</p>`
+    });
+    divListSorted.innerHTML = list.join("/n");
+}
 
 
-var divListSorted = document.getElementById('coffees'); /* Tried a sort here too*/
-var submitButton = document.getElementById('submit');
-var roastSelection = document.getElementById('roast-selection');
-var roastAdd = document.getElementById('roast-selection-pick');
-var roastInput = document.getElementById('pick-new')
 
-divListSorted.innerHTML = renderCoffees(coffees);/*tried a sort here, annnnnd it didnt work*/
 
-submitButton.addEventListener('click', updateCoffees);
 
-roastSelection.addEventListener('change', updateCoffees);
 
-roastInput.addEventListener('input', filterCoffees);
 
-roastAdd.addEventListener('keyup', filterCoffees);
 
-// How to sort objects in ascending order:
+
+
+
+
+// function renderCoffee(coffee) {
+//     var html = '<div class="coffee">';
+//     // html += '<lh>' + coffee.id + '</lh>';
+//     html += '<h3>' + coffee.name + '</h3>';
+//     html += '<p>' + coffee.roast + '</p>';
+//     html += '</div>';
+//     return html;
+// }
+// function renderCoffees(coffees) {
+//     var html = '';
+//     for(var i = coffees.length - 1; i >= 0; i--) {
+//         html += renderCoffee(coffees[i]); /*trying a sort here but didn't work*/
+//     }
+//     return html;
+// }
+// function plshlpme(){
+// coffees.forEach((coffee) =>{
+// let CoffeesName = [];
+// let selectedName = nameSelection.value;
+// if(coffee.name.toLowerCase() === selectedName.toLowerCase()) {
+//     CoffeesName.push(coffee);
+//     console.log(selectedName);
+// }
+// return CoffeesName;
+// });
+// }
+//
+//
+//
+// function updateName(e) {
+//     var filteredName = plshlpme();
+//     divListSorted.innerHTML = renderCoffees(filteredName); /*Tried an array sort here too*/
+// }
+//
+//
+//
+// function filterCoffees() {
+//     var selectedRoast = roastSelection.value;
+//     let filteredCoffees = [];
+//     coffees.forEach(function(coffee) {
+//         if (coffee.roast === selectedRoast) {
+//             filteredCoffees.push(coffee);
+//         }
+//     });
+//     return filteredCoffees
+// }
+//
+//
+//
+// function updateCoffees(e) {
+//     var filteredCoffees = filterCoffees()
+//     divListSorted.innerHTML = renderCoffees(filteredCoffees); /*Tried an array sort here too*/
+// }
+
 function ascending (a, b) {
     if (a.name > b.name) {
         return -1;
@@ -83,5 +120,5 @@ function ascending (a, b) {
     }
     return 0;
 }
-
 coffees.sort(ascending);
+divListSorted.innerHTML = renderCoffeeList(coffees);
