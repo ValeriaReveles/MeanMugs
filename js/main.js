2570dd5eae2fb0c0587e12d1eb817be96b63eace
ValeriaReveles <VALERIA.REVELES@associates.cisa.dhs.gov>
2023-01-23 16:44:02 -0600

    "Further edits to CRUD functionality"

--- js/main.js
+++ js/main.js
@@ -3,12 +3,9 @@ var divList = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 var selectedCreatedRoast = document.getElementById('roast-selection-new');
-var newMug = document.querySelector('#submit-new');
+var newMugSubmission = document.querySelector('#submit-new');
 var createdName = document.getElementById('roast-vibes-new');
 
-function newRoast(){
-    console.log(`created roast: ${selectedCreatedRoast.value}`);
-}
 
 function renderCoffee(coffee) {
     var html = '<div class="coffee">';
@@ -30,9 +27,10 @@ function renderCoffeeList(coffeeList) {
 function updateCoffeeList(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    var selectedName = inputQuery.value;
     var filteredCoffees = [];
     coffeeList.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast && coffee.name.toLocaleLowerCase().includes(selectedName.toLocaleLowerCase())) {
             filteredCoffees.push(coffee);
         }
     });
@@ -40,22 +38,21 @@ function updateCoffeeList(e) {
     divList.innerHTML = renderCoffeeList(filteredCoffees);
 }
 
-function addNewCoffee(e){
-    var newCoffee = {id: selectedCreatedRoast.value, name: createdName.value, roast: selectedCreatedRoast.value}
-    e.preventDefault();
-    if(newCoffee.name.length === 0 || newCoffee.name.replace(/\s/g,"") === "") {
-        alert('Name Your Mug!');
-    } else {
-        coffeeList.push(newCoffee);
-        console.log(newCoffee.name.length);
-        divList.innerHTML = renderCoffeeList(coffeeList);
-    }
+function addNewCoffee(){
+    let newId = coffeeList.length + 1;
+    let newName = createdName.value;
+    let newRoast = selectedCreatedRoast.value;
+    coffeeList.push({id: newId, name: newName, roast: newRoast});
+    console.log(coffeeList);
+    divList.innerHTML = renderCoffeeList(coffeeList);
+    coffeeList.sort(ascending);
 }
+
 
 //Search functionality
 var inputQuery = document.querySelector("#search");
 
-inputQuery.addEventListener('keyup', function(e){
+inputQuery.addEventListener('keyup', function(){
 
     let userInput = this.value.toLowerCase();
     let matchingCoffees = [];
@@ -70,20 +67,20 @@ inputQuery.addEventListener('keyup', function(e){
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffeeList = [
-    {id: 1, name: '--  Light City  --', roast: 'light'},
-    {id: 2, name: '--  Half City  --', roast: 'light'},
-    {id: 3, name: '--  Cinnamon  --', roast: 'light'},
-    {id: 4, name: '--  City  --', roast: 'medium'},
-    {id: 5, name: '--  American  --', roast: 'medium'},
-    {id: 6, name: '--  Breakfast  --', roast: 'medium'},
-    {id: 7, name: '--  High  --', roast: 'dark'},
-    {id: 8, name: '--  Continental  --', roast: 'dark'},
-    {id: 9, name: '--  New Orleans  --', roast: 'dark'},
-    {id: 10, name: '--  European  --', roast: 'dark'},
-    {id: 11, name: '--  Espresso  --', roast: 'dark'},
-    {id: 12, name: '--  Viennese  --', roast: 'dark'},
-    {id: 13, name: '--  Italian  --', roast: 'dark'},
-    {id: 14, name: '--  French  --', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
 ];
 
 
@@ -93,8 +90,8 @@ var coffeeList = [
 divList.innerHTML = renderCoffeeList(coffeeList);
 
 submitButton.addEventListener('click', updateCoffeeList);
-selectedCreatedRoast.addEventListener('change', newRoast);
-newMug.addEventListener('click', addNewCoffee);
+// selectedCreatedRoast.addEventListener('change', newRoast);
+newMugSubmission.addEventListener('click', addNewCoffee);
 
 //Sort Coffee list in ascending order:
 function ascending (a, b) {
